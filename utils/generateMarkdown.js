733a9522cad13913281922ced72921e7332c3dc6
Alek <alekvalencia2535@gmail.com>
2020-06-03 22:58:57 -0700

    added text and structure to README that will be created by generateMarkdown(). Still need to fix the license part especially in userPrompt().

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,50 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+![License Badge](https://img.shields.io/badge/license-${data.license}-blue)
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+[${data.username}](https://github.com/${data.username})
+
+${data.email}
+
+If you have any questions about the project you can reach me at the above email.
 `;
 }
 
