0357cb51a267c4641a6f91d77c0d800136d09908
Alek <alekvalencia2535@gmail.com>
2020-06-04 22:42:45 -0700

    got license to show on README. needed to press space when choosing a license to get it to show. to solve that problem i added validation to all of the questions to make sure that there is a response submitted to complete the README.

--- index.js
+++ index.js
@@ -22,54 +22,98 @@ const questions = [
 function promptUser() {
     return inquirer.prompt([
         {
-            type: "question",
+            type: "input",
             name: "title",
-            message: questions[0]
+            message: questions[0],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter a title for your project."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "description",
-            message: questions[1]
+            message: questions[1],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter a description for your project."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "install",
-            message: questions[2]
+            message: questions[2],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to describe how to install your project."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "usage",
-            message: questions[3]
+            message: questions[3],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to describe how your project is used."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "contribution",
-            message: questions[4]
+            message: questions[4],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter a how to contribute to your project."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "tests",
-            message: questions[5]
+            message: questions[5],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter how to test your project."
+                } return true;
+            }
         },
         {
             type: "checkbox",
             name: "license",
             message: questions[6],
-            choices: ["MIT", "None", "GNU GPL v3", "Apache", "ISC"]
+            choices: ["MIT", "None", "GNU GPL v3", "Apache", "ISC"],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to press space on one of the options."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "username",
-            message: questions[7]
+            message: questions[7],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter your username for GitHub."
+                } return true;
+            }
         },
         {
-            type: "question",
+            type: "input",
             name: "email",
-            message: questions[8]
+            message: questions[8],
+            validate: answer => {
+                if (answer.length < 1) {
+                    return "You need to enter your email."
+                } return true;
+            }
         }
         
     ])
-    // .then(answers => console.log(answers))
 }
 
 // function to write README file
@@ -83,7 +127,7 @@ function writeToFile(fileName, data) {
 function init() {
         promptUser()
         .then( data => {
-            return writeToFile("README.md", data)
+            return writeToFile("README2.md", data)
         })
         .then( () => console.log("Successfully created a README.md!") )
         .catch( error => console.log(error) )
